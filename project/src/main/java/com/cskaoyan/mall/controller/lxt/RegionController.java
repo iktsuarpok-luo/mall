fb5192c046d968266a4c4bfa6d4bbf5f18c62839
com-cskaoyan-luo <199205669@qq.com>
2019-08-16 09:57:50 +0800

    骆晓天 窄化请求修改

---
+++ project/src/main/java/com/cskaoyan/mall/controller/lxt/RegionController.java
@@ -0,0 +1,11 @@
+package com.cskaoyan.mall.controller.lxt;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("admin/region")
+public class RegionController {
+
+}
