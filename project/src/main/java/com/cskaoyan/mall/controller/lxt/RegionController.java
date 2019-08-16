6e3f8f1e7a6fe2af4ffa7469ea08eb6bc59cb80a
com-cskaoyan-luo <199205669@qq.com>
2019-08-16 10:04:36 +0800

    骆晓天 BaseRespModel

--- project/src/main/java/com/cskaoyan/mall/controller/lxt/RegionController.java
+++ project/src/main/java/com/cskaoyan/mall/controller/lxt/RegionController.java
@@ -1,11 +1,16 @@
 package com.cskaoyan.mall.controller.lxt;
 
 
+import com.cskaoyan.mall.bean.BaseRespModel;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("admin/region")
 public class RegionController {
-
+    @RequestMapping("list")
+    public BaseRespModel show(){
+        BaseRespModel resp = new BaseRespModel();
+        return resp;
+    }
 }
