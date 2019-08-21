dfdc67e1f1251f3fce5b85cc61a8bd203fc2af13
Your Name <you@example.com>
2019-08-21 20:39:17 +0800

    李希双 8月21 07.32 完成系统管理所有功能

---
+++ project/src/main/java/com/cskaoyan/mall/Aop/lxs/LogAspect.java
@@ -0,0 +1,76 @@
+package com.cskaoyan.mall.Aop.lxs;
+import com.cskaoyan.mall.bean.Log;
+import com.cskaoyan.mall.service.lxs.AdminService;
+import com.cskaoyan.mall.util.AnnotationLog;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+@Aspect
+public class LogAspect {
+    @Autowired
+    AdminService adminService;
+    Log log = new Log();
+
+    @Pointcut("@annotation(com.cskaoyan.mall.util.AnnotationLog)")
+    public void pointcut() {
+    }
+    @Before("pointcut()")
+    public void Before(JoinPoint point){
+           Object[] args = point.getArgs();
+        if(args.length>1){
+            log.setResult(args[0].toString());
+        }else {
+            String string = args[0].toString();
+            String[] split = string.split("'");
+            log.setResult(split[1]);
+        }
+    }
+    @AfterReturning(value = "pointcut()&&@annotation(alog)", returning = "a")
+    public void myAfterReturning(Object a, AnnotationLog alog) throws UnknownHostException {
+        /*Subject subject = SecurityUtils.getSubject();
+        User user = (User) subject.getPrincipal();
+        String username = user.getUsername();*/
+        String action = alog.value();
+        String address = InetAddress.getLocalHost().getHostAddress();
+        Map b= (Map) a;
+        String errmsg = (String)b.get("errmsg");
+        int errno = (int) b.get("errno");
+        //log.setAdmin(username);
+        log.setType(1);
+        log.setAction(action);
+        log.setAddTime(new Date());
+        log.setUpdateTime(new Date());
+        log.setIp(address);
+        if ("成功".equals(errmsg)){
+            log.setStatus(true);
+        }else {
+            log.setStatus(false);
+        }
+        if(errno!=0){
+            log.setStatus(false);
+        }else {
+            log.setStatus(true);
+        }
+        adminService.logInsert(log);
+        System.out.println("after-returing");
+    }
+    @AfterThrowing(value = "pointcut()",throwing = "a")
+    public void MyAfterThrowing(Exception a){
+        System.out.println("throwing");
+    }
+
+
+    @After("pointcut()")
+    public void after(){
+
+        System.out.println("after");
+    }
+}
