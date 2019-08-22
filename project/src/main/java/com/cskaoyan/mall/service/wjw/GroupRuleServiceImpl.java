78cd7f7418d41efb38ee286f54f4cf1bee9b4b38
Your Name <you@example.com>
2019-08-22 17:36:03 +0800

    "李希双 commit 文件上传"

--- project/src/main/java/com/cskaoyan/mall/service/wjw/GroupRuleServiceImpl.java
+++ project/src/main/java/com/cskaoyan/mall/service/wjw/GroupRuleServiceImpl.java
@@ -90,9 +90,9 @@ public class GroupRuleServiceImpl implements GroupRuleService{
 
     @Override
     public List<Grouponrules> getLimitList(int i, String id, String desc) {
-
-        grouponrulesMapper.selectLimitList();
-        grouponrulesList.subList(0,i);
-        return grouponrulesList;
+//        grouponrulesMapper.selectLimitList();
+//        grouponrulesList.subList(0,i);
+//        return grouponrulesList;
+        return null;
     }
 }
